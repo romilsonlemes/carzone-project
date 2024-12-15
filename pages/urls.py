0a20876796d43b94b9dce0804d22d8356c019f50
romilsonlemes <romilsonlemes@gmail.com>
2024-12-15 03:01:27 -0700

    Created and configured navigation between pages

--- pages/urls.py
+++ pages/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cars', views.cars, name='cars'),
+    path('carDetails', views.carDetails, name='carDetails'),
+    path('about', views.about, name='about'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
 ]
