8646f60cce26989e87f6fad4d7327cd21d38c118
romilsonlemes <romilsonlemes@gmail.com>
2025-01-02 15:46:42 -0700

    Was implemented the instructions about video 61

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard'),
+    path('accounts/', include('allauth.urls')),
 ]
--- carzone/settings.py
+++ carzone/settings.py
@@ -55,6 +55,18 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
 ]
 
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
