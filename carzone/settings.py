7ef65b70277126e5ca32d7eb9edab24e68fe3a36
romilsonlemes <romilsonlemes@gmail.com>
2025-01-03 00:55:38 -0700

    Was implemented the instructions about video 66 - Capturing Inquiry Data & SAve into Database

--- carzone/settings.py
+++ carzone/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
